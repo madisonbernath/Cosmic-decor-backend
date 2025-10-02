f2e2712db490ebd46e03e3869143e2714da52632
madisonbernath <madisonbernath@icloud.com>
2025-10-01 23:36:52 -0400

    Update Tally-webhook.js

--- Tally-webhook.js
+++ Tally-webhook.js
@@ -1,10 +1,1 @@
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" })
-  }
 
-  console.log("Tally sent:", req.body)
-
-  // For now just echo back the form data
-  res.status(200).json({ ok: true, received: req.body })
-}
